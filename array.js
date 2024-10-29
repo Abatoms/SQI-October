2bb5251b05b9c02646c4922862d68f759680cfc6
Bolarinwa Ahmed <bolarinwaahmed22@gmail.com>
2024-10-29 10:08:06 +0100

    Other Array methods

--- array.js
+++ array.js
@@ -71,3 +71,115 @@ console.log(clubs);
 const goodClubs = clubs.slice(0, 3);
 console.log(goodClubs);
 console.log(clubs);
+
+// Array search methods
+// 1. Index of Method
+const kopaTrophy = [
+  "GOAT",
+  "Isco",
+  "Mbappe",
+  "Bellignham",
+  "Pedri",
+  "Gavi",
+  "YAMAL",
+  "YAMAL",
+];
+
+console.log(kopaTrophy);
+
+console.log(kopaTrophy.indexOf("Baby Goat"));
+console.log(kopaTrophy.indexOf("GOAT"));
+console.log(kopaTrophy.indexOf("Mbappe"));
+
+// 2. Last Index of Method
+console.log(kopaTrophy.lastIndexOf("YAMAL"));
+
+// 3. Includes Method
+console.log(kopaTrophy.includes("Gavi"));
+console.log(kopaTrophy.includes("Baby Goat"));
+
+// 4. Find Method
+console.log("Find Method");
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function getNumbersGreaterThanFive(number) {
+  return number > 5;
+}
+
+// console.log(getNumbersGreaterThanFive(6));
+// console.log(getNumbersGreaterThanFive(3));
+
+console.log(numbers.find(getNumbersGreaterThanFive)); // 6
+console.log(numbers.find((number) => number > 8)); // 9
+console.log(
+  numbers.find(function (number) {
+    return number > 9;
+  })
+); // 10
+
+// const check = (number) => number > 8;
+
+const names = [
+  "Jumoke",
+  "Biodun",
+  "Shade",
+  "Motun",
+  "Bisola",
+  "Adeola",
+  "Bolutife",
+];
+
+function checkName(name) {
+  return name.startsWith("M");
+}
+
+console.log(checkName("Bolutife"));
+console.log(checkName("Jumoke"));
+console.log(names.find(checkName));
+console.log(names.find((name) => name.startsWith("A")));
+
+// 5. Find Index Method
+console.log("Find Index Method");
+console.log(names.findIndex(checkName));
+console.log(names.findIndex((name) => name.startsWith("A")));
+
+// Arrsy Sort Method
+console.log("Array Sort Method");
+// 1. sort method
+console.log(names.sort());
+const numbersToSort = [10, 5, 3, 7, 2, 8, 1, 6, 4, 9, 200, 30, 1000];
+console.log(numbersToSort.sort());
+console.log(numbersToSort.sort((a, b) => a - b));
+
+// 2. reverse method
+console.log(names.reverse());
+console.log(numbersToSort.reverse());
+
+// 6. Filter Method
+console.log("Filter Method");
+const myBabes = names.filter((name) => name.startsWith("B"));
+console.log(myBabes);
+const studentScores = [40, 50, 60, 70, 80, 90, 100];
+const excellentStudentsScores = studentScores.filter((score) => score > 70);
+
+console.log(excellentStudentsScores);
+const madridSupportersScore = studentScores.filter((score) => score <= 50);
+console.log(madridSupportersScore);
+
+// Array Iteration Methods
+
+// 1. forEach Method
+console.log("ForEach Method");
+const productsDiv = document.getElementById("products");
+const products = ["Shoe", "Bag", "Shirt", "Trouser", "Cap", "Wristwatch"];
+
+products.forEach((item) => {
+  console.log(item);
+  productsDiv.innerHTML += `<p>${item}</p>`;
+});
+
+// 2. Map Method
+console.log("Map Method");
+const numbersToSquare = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const squaredNumbers = numbersToSquare.map((number) => number ** 2);
+console.log(numbersToSquare);
+console.log(squaredNumbers);
