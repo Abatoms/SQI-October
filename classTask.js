f9affe51a9968450bd0f682d1881a2b9a1f7e864
Bolarinwa Ahmed <bolarinwaahmed22@gmail.com>
2024-10-30 09:57:01 +0100

    Maths and class task

---
+++ classTask.js
@@ -0,0 +1,21 @@
+/*
+Question 1: Number Guessing Game
+Write a function called numberGuessingGame that:
+
+Generates a random number between 1 and 100 (inclusive).
+Prompts the user to guess the number.
+Checks if the guess is correct:
+If the guess is too low, display "Too low! Try again."
+If the guess is too high, display "Too high! Try again."
+If the guess is correct, display "Congratulations! You guessed the number in X attempts!" where X is the number of attempts taken.
+The function should continue to prompt the user until they guess the correct number.
+*/
+
+/*
+    Question 2: Palindrome Checker
+Write a function named findPalindromes that takes an array of strings and returns a new array containing only the palindromes from the original array. A palindrome is a word that reads the same backward as forward.
+
+Example:
+findPalindromes(["madam", "apple", "racecar", "hello", "civic"]);
+// Output: ["madam", "racecar", "civic"]
+*/
---
+++ maths.js
@@ -0,0 +1,53 @@
+console.log("Welcome to MAths In JavaScript");
+console.log(Math.E);
+
+console.log(Math.PI);
+
+// Maths Methods
+// 1. Maths.round
+console.log(Math.round(4.7));
+console.log(Math.round(4.2));
+
+// 2. Math.ceil
+console.log(Math.ceil(4.7));
+console.log(Math.ceil(4.2));
+
+// 3. Math.floor
+console.log(Math.floor(4.7));
+console.log(Math.floor(4.2));
+
+// 4. Math.trunc
+console.log(Math.trunc(4.7));
+console.log(Math.trunc(4.2));
+
+// 5. Math.sign
+console.log(Math.sign(0));
+
+// 6. Math.pow
+console.log(Math.pow(2, 3)); // 2 raised to the power of 3
+console.log(Math.pow(3, 2)); // 3 raised to the power of 2
+
+// 7. Math.sqrt
+console.log(Math.sqrt(144));
+
+// 8. Math.cbrt
+console.log(Math.cbrt(27));
+
+// 9. Math.abs
+console.log(Math.abs(-4.7));
+
+// 10. Math.min
+console.log(Math.min(0, 150, 30, 20, -8, -200));
+
+// 11. Math.max
+console.log(Math.max(0, 150, 30, 20, -8, -200));
+
+// 12. Math.random
+console.log(Math.random());
+
+//  generate a random number between 1 and 40
+console.log(Math.trunc(Math.random() * 40) + 1);
+
+// Difference between floor and trunc
+console.log(Math.floor(-3.5));
+console.log(Math.trunc(-3.5));
